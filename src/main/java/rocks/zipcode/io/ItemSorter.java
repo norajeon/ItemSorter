69153be16224c5d16042890af3559f6607561f67
nora <sxj042@gmail.com>
2023-03-25 17:51:04 -0400

    working on it

--- src/main/java/rocks/zipcode/io/ItemSorter.java
+++ src/main/java/rocks/zipcode/io/ItemSorter.java
@@ -6,10 +6,13 @@ import java.util.Comparator;
  * @author leon on 30/01/2019.
  */
 public class ItemSorter {
+    Item[] items;
     public ItemSorter(Item[] items) {
+        this.items = items;
     }
 
     public Item[] sort(Comparator<Item> comparator) {
+
         return null;
     }
 }
